168ce1630db17279bf5b1f0b2527e1e28c3c8c1f
Sandy Small <sandymsmall@gmail.com>
2020-12-21 20:28:40 -0800

    Added poly, swapped in faster powf() approx., corrected Amount display

--- src/Exponent.cpp
+++ src/Exponent.cpp
@@ -24,12 +24,23 @@ struct Exponent : Module
 	Exponent()
 	{
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS);
-		configParam(Exponent::AMOUNT_PARAM, -5.0, 5.0, 0.0, "Amount");
+		configParam(Exponent::AMOUNT_PARAM, -1.0, 1.0, 0.0, "Amount", "", 2);
 		configParam(Exponent::SCALE_PARAM, -1.0, 1.0, 1.0, "Mod");
 		configParam(Exponent::RANGE_PARAM, 0.0, 1.0, 0.0, "Range", "V", 0.0, 5.0, 5.0);
 	}
 
 	void process(const ProcessArgs &args) override;
+
+	// faster powf() approximation cribbed from martin.ankerl.com/2007/10/04/optimized-pow-approximation-for-java-and-c-c/
+	double fastPow(double a, double b) {
+	    union {
+	        double d;
+	        int x[2];
+	    } u = { a };
+	    u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
+	    u.x[0] = 0;
+	    return u.d;
+	}
 
 	// For more advanced Module features, read Rack's engine.hpp header file
 	// - dataToJson, dataFromJson: serialization of internal data
@@ -40,30 +51,31 @@ struct Exponent : Module
 
 void Exponent::process(const ProcessArgs &args)
 {
-	float input = inputs[MAIN_INPUT].getVoltage();
-	const bool negativeInput = input < 0.0f;
-
     bool mode5V = (params[RANGE_PARAM].getValue() == 0.0f);
-    if(mode5V) input = clamp(input, -5.0f, 5.0f) * 0.2f;
-	else input = clamp(input, -10.0f, 10.0f) * 0.1f;
-	input = std::abs(input);
 
-    float exponent = params[AMOUNT_PARAM].getValue() + (inputs[AMOUNT_INPUT].getVoltage() * params[SCALE_PARAM].getValue());
-    exponent = clamp(exponent, -5.0f, 5.0f) * 0.2f;
+	int channels = inputs[MAIN_INPUT].getChannels();
+	for (int c = 0; c < channels; c++) {
+		float input = inputs[MAIN_INPUT].getPolyVoltage(c);
+		const bool negativeInput = input < 0.0f;
 
-	if(exponent < 0)
-	{
-		exponent = 1.0f - (exponent * -0.5f);
+	    if(mode5V) input = clamp(input, -5.0f, 5.0f) * 0.2f;
+		else input = clamp(input, -10.0f, 10.0f) * 0.1f;
+		input = std::abs(input);
+
+	    float exponent = params[AMOUNT_PARAM].getValue() + (inputs[AMOUNT_INPUT].getPolyVoltage(c) * (params[SCALE_PARAM].getValue() * 0.2));
+
+		if(exponent < 0) exponent = 1.0f - (exponent * -0.5f);
+		else exponent += 1.0f;
+
+	    float output = fastPow(input, exponent);
+
+		if (negativeInput) output *= -1.0f;
+	    if(mode5V) output *= 5.0f;
+	    else output *= 10.0f;
+
+	    outputs[MAIN_OUTPUT].setVoltage(output, c);
 	}
-	else exponent += 1.0f;
-
-    float output = powf(input, exponent);
-
-	if (negativeInput) output *= -1.0f;
-    if(mode5V) output *= 5.0f;
-    else output *= 10.0f;
-
-    outputs[MAIN_OUTPUT].setVoltage(output);
+	outputs[MAIN_OUTPUT].setChannels(channels);
 }
 
 struct CKSSRot : SvgSwitch {
